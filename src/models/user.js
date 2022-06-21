927d4cd14a01ca384e095b1e50cbbf76636024d2
acadevy <rashyolat@gmail.com>
2022-06-21 21:24:49 +0100

    worked on the user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,13 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    }
+})
