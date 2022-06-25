e78dfbaada3bc58593e286d486de60edaa88388e
acadevy <rashyolat@gmail.com>
2022-06-25 12:10:07 +0100

    worked on the app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-const express = require("express");
+const express = require('express');
+require('./db/mongoose');
+const userRoute = require('./routes/user');
+const taskRoute = require('./routes/task');
+
 const app = express();
 
-require('./db/mongoose');
-require('dotenv').config();
+app.use(express.json());
+app.use(userRoute);
+app.use(taskRoute);
 
+app.use((req, res) => {
+  return res.status(404).json({ error: 'Not found' });
+});
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  return res.status(500).json({ error: 'Internal error' });
+});
 
-const port = process.env.PORT;
-
-
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-})
+module.exports = app;
